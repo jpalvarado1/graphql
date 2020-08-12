0dbb04d266ed11669eb5d47c2618160832c43125
IOS_Casinos <30815111+jpalvarado1@users.noreply.github.com>
2020-08-12 17:55:19 -0500

    implement user model validation

--- lib/graphql/accounts/user.ex
+++ lib/graphql/accounts/user.ex
@@ -3,11 +3,13 @@ defmodule Graphql.Accounts.User do
   import Ecto.Changeset
 
   schema "users" do
-    field(:email, :string)
+    field(:email, :string, unique: true)
     field(:name, :string)
     field(:password_hash, :string)
-    field(:role, :string)
-    field(:username, :string)
+    field(:password, :string, virtual: true)
+    field(:password_confirmation, :string, virtual: true)
+    field(:role, :string, default: "user")
+    field(:username, :string, unique: true)
 
     timestamps()
   end
@@ -15,7 +17,19 @@ defmodule Graphql.Accounts.User do
   @doc false
   def changeset(user, attrs) do
     user
-    |> cast(attrs, [:name, :username, :email, :password_hash, :role])
-    |> validate_required([:name, :username, :email, :password_hash, :role])
+    |> cast(attrs, [:name, :username, :email, :password, :password_confirmation, :role])
+    |> validate_required([:name, :username, :email, :password, :password_confirmation, :role])
+    |> validate_format(:email, ~r/@/)
+    |> update_change(:email, &String.downcase(&1))
+    |> validate_length(:username, min: 6, max: 20)
+    |> validate_length(:password, min: 6, max: 16)
+    |> validate_confirmation(:password)
+    |> unique_contraint(:username)
+    |> unique_contraint(:email)
+    |> hash_password
+  end
+
+  defp hash_password(changeset) do
+    changeset    
   end
 end
