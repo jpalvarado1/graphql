592e66ac5d089b8a5c269bfb1e6eba1e4ca014f9
IOS_Casinos <30815111+jpalvarado1@users.noreply.github.com>
2020-08-17 09:44:32 -0500

    setup schema file

---
+++ lib/graphql_web/schema.ex
@@ -0,0 +1,15 @@
+defmodule GraphqlWeb.Schema do
+    use Absinthe.Schema
+    #import_types GraphqlWeb.Schema.ContentTypes
+  
+    #alias GraphqlWeb.Resolvers
+  
+    query do
+    end
+  
+    # mutation do
+    # end
+
+    # subscription do
+    # end
+  end
--- mix.exs
+++ mix.exs
@@ -43,7 +43,8 @@ defmodule Graphql.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:argon2_elixir, "~> 2.0"}
+      {:argon2_elixir, "~> 2.0"},
+      {:absinthe, "~> 1.5", override: true}
     ]
   end
 
